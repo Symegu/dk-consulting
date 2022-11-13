5d6393195efb41be16ad8501fe2fa0c09b8066b6
Symegu <89914120+Symegu@users.noreply.github.com>
2022-11-13 16:55:49 +0300

    upd adaptive mainservices

--- dk-consulting/src/components/UI/GreyLine.jsx
+++ dk-consulting/src/components/UI/GreyLine.jsx
@@ -2,12 +2,12 @@ import picture from "../../assets/Main/Presentation.svg"
 
 export const GreyLine = (props) => {
     return (
-        <div className="bg-greylinebg py-[25px] flex flex-col items-center justify-center bg-no-repeat bg-cover bg-center">
+        <div className="bg-greylinebg py-[25px] flex flex-col items-center justify-center bg-no-repeat bg-cover bg-center mx-auto">
             <div className="flex items-center mb-[17px]">
                 <img src={props.pic ? picture : ""} alt="" />
                 <h3 className="littleTitle text-white ml-[20px]">{props.heading}</h3>
             </div>
-            <p className={props.bigpar ? "max-w-[1290px] text-center text-lightgrey font-extralight text-[35px] leading-[49px]" : "max-w-[1290px] par text-center text-lightgrey"}>{props.par}</p>
+            <p className={props.bigpar ? "max-w-[1290px] text-center text-lightgrey font-extralight text-[35px] leading-[49px] lg:max-w-[940px] md:max-w-[690px] md:text-start" : "max-w-[1290px] par text-center text-lightgrey lg:max-w-[940px] md:max-w-[690px] md:text-start"}>{props.par}</p>
         </div>
     )
 }
